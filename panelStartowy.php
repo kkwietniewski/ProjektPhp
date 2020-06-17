e1c70f8704295b66189f58c5c0f6c133019e8473
Monika <Monika@LAPTOP-7UJT8AEP>
2020-06-17 20:16:08 +0200

    nav

---
+++ panelStartowy.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang=pl>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Aplikacja do zarządzania sklepem internetowym</title>
+</head>
+<body>
+                <nav class="navbar-expand-xl navbar-light bg-light p-2">
+                    <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-2">
+                        <li class="nav-item">
+                            <a class="nav-link">Panel startowy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Asortyment</a>
+                        </li> 
+                        <li class="nav-item">
+                            <a class="nav-link">Dodaj produkty</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Zamówienia</a>
+                        </li>
+                        </div> 
+                    </ul>
+                </nav>
+
+        
+  
+
+
+<div class="container-fluid mt-5">
+        <div class="row">
+
+            <div class="col-8">
+                
+                <table class="table table-striped table-dark border border-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">Data</th>
+                           <th scope="col">Szczegóły zamówienia</th>
+                           <th scope="col">Klient</th>
+                            <th scope="col">Wartość</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr> 
+                            <td scope="row">02-01-2012</th>
+                            <td scope="row">zamówienie nr 124243</th>
+                            <td scope="row">Jan Nowak</th>
+                            <td scope="row">213,54 zł</th>
+                            <td scope="row">opłacone</th>
+                        </tr>
+                     <tr> 
+                            <td scope="row">02-01-2012</th>
+                            <td scope="row">zamówienie nr 124243</th>
+                            <td scope="row">Jan Nowak</th>
+                            <td scope="row">213,54 zł</th>
+                            <td scope="row">opłacone</th>
+                        </tr>
+                     <tr> 
+                            <td scope="row">02-01-2012</th>
+                            <td scope="row">zamówienie nr 124243</th>
+                            <td scope="row">Jan Nowak</th>
+                            <td scope="row">213,54 zł</th>
+                            <td scope="row">opłacone</th>
+                        </tr>
+                     <tr> 
+                            <td scope="row">02-01-2012</th>
+                            <td scope="row">zamówienie nr 124243</th>
+                            <td scope="row">Jan Nowak</th>
+                            <td scope="row">213,54 zł</th>
+                            <td scope="row">opłacone</th>
+                        </tr>
+                    
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div class="col-4 ">
+
+            <div class="jumbotron">
+                <h5 class="display-5">Oczekujące na obsługę</h5>
+                <hr class="my-4">
+ 
+            </div>
+
+            </div>
+            
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
