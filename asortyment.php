c6c9961ee196794c9be5c0fb5fef99e82debc505
Kacper Kwietniewski <kkwietniewski@office.cdv.pl>
2020-06-17 21:40:48 +0200

    asortyment

---
+++ asortyment.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang=pl>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Aplikacja do zarządzania sklepem internetowym</title>
+</head>
+<body>
+                <nav class="navbar-expand-xl navbar-light bg-light p-2">
+                    <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-2">
+                        <li class="nav-item">
+                            <a class="nav-link">Panel startowy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Asortyment</a>
+                        </li> 
+                        <li class="nav-item">
+                            <a class="nav-link">Dodaj produkty</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Zamówienia</a>
+                        </li>
+                        </div> 
+                    </ul>
+                </nav>
+
+        
+  
+
+
+<div class="container-fluid mt-5">
+        <div class="row">
+
+            <div class="col-8">
+                
+            <div class="md-form active-blue-2 mb-3">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </div>
+            
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
