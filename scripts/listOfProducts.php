2c0f99833ef56f5a22a7bd4b2b15faa7545b44bf
dMichalak <48916443+dMichalak1@users.noreply.github.com>
2020-06-20 16:00:53 +0200

    produkty

--- scripts/listOfProducts.php
+++ scripts/listOfProducts.php
@@ -1,28 +1,46 @@
 <?php
     require_once './scripts/connect.php';
 
-    $sql = "SELECT id,symbol,nazwa,cena,obrazek_url,stan,waga,kategoria,producent,znacznik AS wartosc FROM produkty ORDER BY id;
-
-    $result = mysqli_query($conn, $sql);
-
+        $sql = "SELECT id,symbol,nazwa,cena,obrazek_url,stan,waga,kategoria,producent,znacznik AS wartosc FROM produkty ORDER BY id";
+        $result = mysqli_query($conn, $sql);
     echo <<<TAB
     <table class="table table-striped table-light ">
-    <thead>
+        <thead>
             <tr>
-                <th scope="col">id</th>
+                <th scope="col">Id</th>
                 <th scope="col">Miniatura</th>
-                <th scope="col">symbol</th>
-                <th scope="col">nazwa</th>
-                <th scope="col">cena</th>
-                <th scope="col">stan</th>
-                <th scope="col">waga</th>
-                <th scope="col">kategoria</th>
-                <th scope="col">producent</th>
-                
-                
+                <th scope="col">Symbol</th>
+                <th scope="col">Nazwa</th>
+                <th scope="col">Cena</th>
+                <th scope="col">Stan</th>
+                <th scope="col">Waga</th>
+                <th scope="col">Kategoria</th>
+                <th scope="col">Producent</th>
             </tr>
-    </thead>
-       
+        </thead>
+        <tbody>
 TAB;
-    
+    while ($row = mysqli_fetch_assoc($result)) {
+    echo <<<TAB
+        
+            <tr> 
+                <td scope="row">$row[id]</td>
+                <div class="card d-flex justify-content-center" style="width:50px;">
+                <img class="card-img-top mx-auto mt-3 mb-0" src="$row[obrazek_url]">
+                <td scope="row">$row[symbol] </td>
+                <td scope="row">$row[nazwa]</td>
+                <td scope="row">$row[cena]</td>
+                <td scope="row">$row[stan]</td>
+                <td scope="row">$row[waga]</td>
+                <td scope="row">$row[kategoria]</td>
+                <td scope="row">$row[producent]</td>
+            </tr>
+
+TAB;
+    }
+    echo <<<TABLE
+            
+    </tbody>
+    </table>
+TABLE;
 ?>
