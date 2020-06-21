37a842cf3c02c1f68e1f86f26497eba176208266
Monika <Monika@LAPTOP-7UJT8AEP>
2020-06-21 11:59:14 +0200

    n

--- scripts/sortKategory.php
+++ scripts/sortKategory.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     require_once './scripts/connect.php';
     $sql="SELECT kategoria_id, kategoria FROM kategoria"; 
     $result = mysqli_query($conn, $sql);
@@ -8,14 +7,4 @@
         <option value="$row[kategoria_id]">$row[kategoria]</option>
 OPTIONS; 
     }
-=======
-   require_once './scripts/connect.php';
-  $sql="SELECT kategoria_id, kategoria FROM kategoria"; 
-  $result = mysqli_query($conn, $sql);
-  while ($row = mysqli_fetch_assoc($result)) {
-echo<<<OPTIONS
-    <option value="$row[kategoria_id]">$kategoria</option>
-OPTIONS;
-}
->>>>>>> 1dcee3c89e5f5d5195d4ee3d6893d921327ba937
 ?>
